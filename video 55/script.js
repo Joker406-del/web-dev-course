f70d031a19325d6de2409595847406aaef8d26ef
Joker406-del <uzairkhlaid@gmail.com>
2025-05-31 05:54:00 -0700

    Add files via upload

---
+++ video 55/script.js
@@ -0,0 +1,11 @@
+console.log("Hi my name is harry")
+let a = 6
+let b = 5
+let c = "harry"
+console.log(a + b + c)
+console.log(c + 4,typeof c)
+const obj ={
+    name:"uzair",
+    passion:"coding",
+}
+console.log(obj)
